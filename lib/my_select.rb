742237602378334c0e73abf030e5b872642b85dc
AJ Guiles <ashiguiles@gmail.com>
2019-03-03 08:19:26 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,7 +1,12 @@
 def my_select(collection)
   i = 0 
-  
- 
+ selected = [] 
+ while i < collection.length 
+    if yield(collection[i]) 
+      selected << collection[i]
+    end 
+    i = i + 1
+
 end
 
 
