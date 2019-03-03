0c7538a8a7993e56c178b7aa8b692183352a41fd
AJ Guiles <ashiguiles@gmail.com>
2019-03-03 08:24:06 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -8,18 +8,6 @@ def my_select(collection)
     i += 1 
   end
   selected
-  
 end
 
 
-def my_select(collection)
-i = 0
-selected_items = []
-while i < collection.size
-  if yield(collection[i]) ##if method that takes this block returns true
-    selected_items << collection[i] ##put the item into selected_items
-  end
-  i = i + 1 ##increase iterator
-  end
-  selected_items ##return selected items
-end
